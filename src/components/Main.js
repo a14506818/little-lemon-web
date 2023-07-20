69e482c8000f5f1c2d43a723bf294ad99dcd5bbc
PC of AppleSheep <a14506819@gmail.com>
2023-07-20 22:06:34 +0800

    setting hero section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import heroImg from '../assets/restaurant.jpg';
+import './Main.css';
 
-const Main = (props) => {
+const Main = () => {
   return (
-    <main>Main{props.children}</main>
+    <main>
+        <div>
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+          <button>Reserve a Table</button>
+        </div>
+        <img src={heroImg} alt='hero image' width='300px'></img>
+    </main>
   )
 }
 
