c42aa9a86acc54ca94978b34802894f2742a6c65
appleSheep <a14506819@gmail.com>
2023-07-23 14:03:53 +0800

    set up testimonial & about css layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,44 +1,62 @@
-import React from 'react'
-import Img from '../assets/restauranfood.jpg';
-import './Footer.css';
+import React from "react";
+import Img from "../assets/restauranfood.jpg";
+import "./Footer.css";
 
 const navList = [
-  'Home',
-  'About',
-  'Menu',
-  'Reservations',
-  'Order Online',
-  'Login'
+  "Home",
+  "About",
+  "Menu",
+  "Reservations",
+  "Order Online",
+  "Login",
 ];
 
 const Footer = () => {
   return (
     <footer>
-      <img src={Img} alt='restauranfood' width='162px' height='279px'/>
-			<div>
-				<h3>Doormat Navigations</h3>
-				<ul>
-					{navList.map(item=>{return <li key={item}><a href=''>{item}</a></li>})}
-				</ul>
-			</div>
-			<div>
-				<h3>Contact</h3>
-				<ul>
-					<li key='Adress'><a href=''>Adress</a></li>
-					<li key='Phone Number'><a href=''>Phone Number</a></li>
-					<li key='Email'><a href=''>Email</a></li>
-				</ul>
-			</div>
-			<div>
-				<h3>Social Media Links</h3>
-				<ul>
-					<li key='Adress'><a href=''>Adress</a></li>
-					<li key='Phone Number'><a href=''>Phone Number</a></li>
-					<li key='Email'><a href=''>Email</a></li>
-				</ul>
-			</div>
+      <img src={Img} alt="restauranfood" width="162px" height="279px" />
+      <div>
+        <h3>Doormat Navigations</h3>
+        <ul>
+          {navList.map((item) => {
+            return (
+              <li key={item}>
+                <a href="">{item}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div>
+        <h3>Contact</h3>
+        <ul>
+          <li key="Adress">
+            <a href="">Adress</a>
+          </li>
+          <li key="Phone Number">
+            <a href="">Phone Number</a>
+          </li>
+          <li key="Email">
+            <a href="">Email</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3>Social Media Links</h3>
+        <ul>
+          <li key="Adress">
+            <a href="">Adress</a>
+          </li>
+          <li key="Phone Number">
+            <a href="">Phone Number</a>
+          </li>
+          <li key="Email">
+            <a href="">Email</a>
+          </li>
+        </ul>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,11 @@
-import React from 'react'
-import heroImg from '../assets/restaurant.jpg';
-import './Main.css';
-import saladImg from '../assets/greek salad.jpg'
-import bruchettaImg from '../assets/bruchetta.svg'
-import lemonImg from '../assets/lemon dessert.jpg'
+import React from "react";
+import heroImg from "../assets/restaurant.jpg";
+import "./Main.css";
+import saladImg from "../assets/greek salad.jpg";
+import bruchettaImg from "../assets/bruchetta.svg";
+import lemonImg from "../assets/lemon dessert.jpg";
+import aboutImg_A from "../assets/Mario and Adrian A.jpg";
+import aboutImg_B from "../assets/Mario and Adrian b.jpg";
 
 const Main = () => {
   return (
@@ -13,10 +15,13 @@ const Main = () => {
           <div>
             <h1>Little Lemon</h1>
             <h2>Chicago</h2>
-            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+            <p>
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout.
+            </p>
             <button>Reserve a Table</button>
           </div>
-          <img src={heroImg} alt='hero image' width='300px'></img>
+          <img src={heroImg} alt="hero image" width="400px"></img>
         </div>
       </div>
       <div className="specialsBG">
@@ -27,37 +32,92 @@ const Main = () => {
           </div>
           <div className="cards">
             <div className="card">
-              <img src={saladImg} alt='salad image' width='100%'/>
-              <div className='cardTitle'>
+              <img src={saladImg} alt="salad image" width="100%" />
+              <div className="cardTitle">
                 <h3>Greek salad</h3>
                 <h4>$12.99</h4>
               </div>
-              <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
-              <h4>Order a delivery</h4>
+              <p>
+                The famous greek salad of crispy lettuce, peppers, olives and
+                our Chicago style feta cheese, garnished with crunchy garlic and
+                rosemary croutons.{" "}
+              </p>
+              <a href="">Order a delivery</a>
             </div>
             <div className="card">
-              <img src={bruchettaImg} alt='salad image' width='100%'/>
-              <div className='cardTitle'>
+              <img src={bruchettaImg} alt="salad image" width="100%" />
+              <div className="cardTitle">
                 <h3>Bruchetta</h3>
                 <h4>$5.99</h4>
               </div>
-              <p>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. </p>
-              <h4>Order a delivery</h4>
+              <p>
+                Our Bruschetta is made from grilled bread that has been smeared
+                with garlic and seasoned with salt and olive oil.{" "}
+              </p>
+              <a href="">Order a delivery</a>
             </div>
             <div className="card">
-              <img src={lemonImg} alt='salad image' width='100%'/>
-              <div className='cardTitle'>
+              <img src={lemonImg} alt="salad image" width="100%" />
+              <div className="cardTitle">
                 <h3>Lemon dessert</h3>
                 <h4>$5.00</h4>
               </div>
-              <p>This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
-              <h4>Order a delivery</h4>
+              <p>
+                This comes straight from grandma’s recipe book, every last
+                ingredient has been sourced and is as authentic as can be
+                imagined.
+              </p>
+              <a href="">Order a delivery</a>
             </div>
           </div>
         </div>
       </div>
+      <div className="testimonialsBG">
+        <div className="testimonials">
+          <h2>Testimonials</h2>
+          <div className="cards">
+            <div className="card">
+              <h3>Rating</h3>
+              <div className="avatar">
+                <img src="" alt="avatar img" />
+                <h4>name</h4>
+              </div>
+              <h4>Review text</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="aboutBG">
+        <div className="about">
+          <div className="textArea">
+            <h2>Little Lemon</h2>
+            <h3>Chicago</h3>
+            <p>
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet. Amet minim mollit
+              non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+              officia consequat duis enim velit mollit.
+            </p>
+          </div>
+          <div className="imgArea">
+            <img
+              src={aboutImg_A}
+              alt="about img A"
+              width="276px"
+              height="338px"
+            />
+            <img
+              src={aboutImg_B}
+              alt="about img B"
+              width="276px"
+              height="338px"
+            />
+          </div>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
