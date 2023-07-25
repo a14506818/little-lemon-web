c9cfcd698cd56d37f0f1d4bdbfb9cd245bb01445
a14506818 <a14506819@gmail.com>
2023-07-25 13:17:24 +0800

    set up formik and yup in booking form

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -9,30 +9,60 @@ import {
     Select,
     Button,
   } from '@chakra-ui/react'
+import { useFormik, Form } from 'formik';
+import * as Yup from 'yup';
 
 const BookingForm = () => {
+    const formik = useFormik({
+        initialValues: {
+			res_date: '',
+            res_time: '',
+            guests: '',
+            occasion: '',
+        },
+		onSubmit: values => {
+			// run submit function
+            alert(JSON.stringify(values, null, 2));
+		},
+		validationSchema: Yup.object().shape({
+            res_date: Yup.string().required(),
+            guests: Yup.number().required().positive().integer(),
+		}),
+    });
+
     return (
-    <FormControl width='400px'>
-        <FormLabel htmlFor='res-date'>Choose date</FormLabel>
-        <Input type='date' id='res-date' />
-        <FormLabel htmlFor='res-time'>Choose time</FormLabel>
-        <Select placeholder='select...' id='res-time'>
-            <option>17:00</option>
-            <option>18:00</option>
-            <option>19:00</option>
-            <option>20:00</option>
-            <option>21:00</option>
-            <option>22:00</option>
-        </Select>
-        <FormLabel htmlFor='guests'>Number of guests</FormLabel>
-        <Input type='number' placeholder='1' min='1' max='10' id='guests'/>
-        <FormLabel htmlFor='occasion'>Occasion</FormLabel>
-        <Select>
-            <option>Birthday</option>
-            <option>Anniversary</option>
-        </Select>
+    <form onSubmit={formik.handleSubmit}>
+        <FormControl isInvalid={!!formik.errors.res_date && formik.touched.res_date}>
+            <FormLabel htmlFor='res-date'>Choose date</FormLabel>
+            <Input type='date' id='res-date' placeholder="Select Date" {...formik.getFieldProps("res_date")} />
+            <FormErrorMessage>{formik.errors.res_date}</FormErrorMessage>
+        </FormControl>
+        <FormControl>
+            <FormLabel htmlFor='res-time'>Choose time</FormLabel>
+            <Select id='res-time' {...formik.getFieldProps("res_time")}>
+                <option>17:00</option>
+                <option>18:00</option>
+                <option>19:00</option>
+                <option>20:00</option>
+                <option>21:00</option>
+                <option>22:00</option>
+            </Select>
+        </FormControl>
+        <FormControl isInvalid={!!formik.errors.guests && formik.touched.guests}>
+            <FormLabel htmlFor='guests'>Number of guests</FormLabel>
+            <Input type='number' placeholder='1' min='1' max='10' id='guests' {...formik.getFieldProps("guests")}/>
+            <FormErrorMessage>{formik.errors.guests}</FormErrorMessage>
+        </FormControl>
+        <FormControl>
+            <FormLabel htmlFor='occasion'>Occasion</FormLabel>
+            <Select {...formik.getFieldProps("occasion")}>
+                <option>None</option>
+                <option>Birthday</option>
+                <option>Anniversary</option>
+            </Select>
+        </FormControl>
         <Button mt={4} colorScheme='yellow' type='submit'>Submit</Button>
-    </FormControl>
+    </form>
     )
 }
 
