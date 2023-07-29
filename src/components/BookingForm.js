6a55a447a4b1eeffa72ee928c94d840b64b8f58e
appleSheep <a14506819@gmail.com>
2023-07-29 17:10:05 +0800

    del space

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,87 +1,118 @@
-import React, { useState, useEffect } from 'react'
-import './BookingForm.css'
+import React, { useState, useEffect } from "react";
+import "./BookingForm.css";
 import {
-    FormControl,
-    FormLabel,
-    FormErrorMessage,
-    Input,
-    Select,
-    Button,
-  } from '@chakra-ui/react'
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import { fetchAPI, submitAPI } from './Api';
-import { useNavigate  } from 'react-router-dom';
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  Select,
+  Button,
+} from "@chakra-ui/react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { fetchAPI, submitAPI } from "./Api";
+import { useNavigate } from "react-router-dom";
 
 const BookingForm = () => {
-    const [availableTimes, setAvailableTimes] = useState([]);
+  const [availableTimes, setAvailableTimes] = useState([]);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const formik = useFormik({
-        initialValues: {
-			res_date: '',
-            res_time: '',
-            guests: '',
-            occasion: '',
-        },
-		onSubmit: values => {
-			// run submit function
-            
-            const formDate = JSON.stringify(values, null, 2);
-            console.log(formDate);
-            if (submitAPI(formDate)) {
-                navigate("/ConfirmedBooking");
-            }else{alert('Submit Failed')};
-		},
-		validationSchema: Yup.object().shape({
-            res_date: Yup.string().required(),
-            res_time: Yup.string().required(),
-            guests: Yup.number().required().positive().integer(),
-            occasion: Yup.string().required(),
-		}),
-    });
+  const formik = useFormik({
+    initialValues: {
+      res_date: "",
+      res_time: "",
+      guests: "",
+      occasion: "",
+    },
+    onSubmit: (values) => {
+      // run submit function
+      const formDate = JSON.stringify(values, null, 2);
+      console.log(formDate);
+      if (submitAPI(formDate)) {
+        navigate("/ConfirmedBooking");
+      } else {
+        alert("Submit Failed");
+      }
+    },
+    validationSchema: Yup.object().shape({
+      res_date: Yup.string().required(),
+      res_time: Yup.string().required(),
+      guests: Yup.number().required().positive().integer(),
+      occasion: Yup.string().required(),
+    }),
+  });
 
-    useEffect(()=>{
-        if(formik.values.res_date === ''){
-            setAvailableTimes(fetchAPI(new Date()));
-        }else{
-            setAvailableTimes(fetchAPI(new Date(formik.values.res_date)));
-        }
-    },[formik.values.res_date])
+  useEffect(() => {
+    if (formik.values.res_date === "") {
+      setAvailableTimes(fetchAPI(new Date()));
+    } else {
+      setAvailableTimes(fetchAPI(new Date(formik.values.res_date)));
+    }
+  }, [formik.values.res_date]);
 
-    return (
+  return (
     <form onSubmit={formik.handleSubmit}>
-        <FormControl isInvalid={!!formik.errors.res_date && formik.touched.res_date}>
-            <FormLabel htmlFor='res-date'>Choose date</FormLabel>
-            <Input type='date' id='res-date' placeholder="Select Date" {...formik.getFieldProps("res_date")} />
-            <FormErrorMessage>{formik.errors.res_date}</FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={!!formik.errors.res_time && formik.touched.res_time}>
-            <FormLabel htmlFor='res-time'>Choose time</FormLabel>
-            <Select id='res-time' placeholder='select...' {...formik.getFieldProps("res_time")}>
-                {availableTimes.map(time=><option key={time}>{time}</option>)}
-            </Select>
-            <FormErrorMessage>{formik.errors.res_time}</FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={!!formik.errors.guests && formik.touched.guests}>
-            <FormLabel htmlFor='guests'>Number of guests</FormLabel>
-            <Input type='number' placeholder='1' min='1' max='10' id='guests' {...formik.getFieldProps("guests")} />
-            <FormErrorMessage>{formik.errors.guests}</FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={!!formik.errors.occasion && formik.touched.occasion}>
-            <FormLabel htmlFor='occasion'>Occasion</FormLabel>
-            <Select id='occasion'  placeholder='select...' {...formik.getFieldProps("occasion")}>
-                <option>None</option>
-                <option>Birthday</option>
-                <option>Anniversary</option>
-                <option>Other</option>
-            </Select>
-            <FormErrorMessage>{formik.errors.occasion}</FormErrorMessage>
-        </FormControl>
-        <Button mt={4} colorScheme='yellow' type='submit'>Submit</Button>
+      <FormControl
+        isInvalid={!!formik.errors.res_date && formik.touched.res_date}
+      >
+        <FormLabel htmlFor="res-date">Choose date</FormLabel>
+        <Input
+          type="date"
+          id="res-date"
+          placeholder="Select Date"
+          {...formik.getFieldProps("res_date")}
+        />
+        <FormErrorMessage>{formik.errors.res_date}</FormErrorMessage>
+      </FormControl>
+      <FormControl
+        isInvalid={!!formik.errors.res_time && formik.touched.res_time}
+      >
+        <FormLabel htmlFor="res-time">Choose time</FormLabel>
+        <Select
+          id="res-time"
+          placeholder="select..."
+          {...formik.getFieldProps("res_time")}
+        >
+          {availableTimes.map((time) => (
+            <option key={time}>{time}</option>
+          ))}
+        </Select>
+        <FormErrorMessage>{formik.errors.res_time}</FormErrorMessage>
+      </FormControl>
+      <FormControl isInvalid={!!formik.errors.guests && formik.touched.guests}>
+        <FormLabel htmlFor="guests">Number of guests</FormLabel>
+        <Input
+          type="number"
+          placeholder="1"
+          min="1"
+          max="10"
+          id="guests"
+          {...formik.getFieldProps("guests")}
+        />
+        <FormErrorMessage>{formik.errors.guests}</FormErrorMessage>
+      </FormControl>
+      <FormControl
+        isInvalid={!!formik.errors.occasion && formik.touched.occasion}
+      >
+        <FormLabel htmlFor="occasion">Occasion</FormLabel>
+        <Select
+          id="occasion"
+          placeholder="select..."
+          {...formik.getFieldProps("occasion")}
+        >
+          <option>None</option>
+          <option>Birthday</option>
+          <option>Anniversary</option>
+          <option>Other</option>
+        </Select>
+        <FormErrorMessage>{formik.errors.occasion}</FormErrorMessage>
+      </FormControl>
+      <Button mt={4} colorScheme="yellow" type="submit">
+        Submit
+      </Button>
     </form>
-    )
-}
+  );
+};
 
-export default BookingForm
+export default BookingForm;
